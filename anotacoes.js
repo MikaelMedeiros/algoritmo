c0582e3297f833a3ddea0953b1a3be1601531122
Lucas <Lucas>
2022-02-16 20:42:56 -0300

    third commit

--- anotacoes.js
+++ anotacoes.js
@@ -13,3 +13,11 @@
  * 3 - selecionar a planta a ser regada
  * 4 - regar a planta selecionada  
  */
+/**
+ * algoritomo para iniciar uma partida de dota
+ * 1 - ligar o computador
+ * 2 - abrir a steam
+ * 3 - instalar o jogo
+ * 4 - iniciar o jogo
+ * 5 - colocar para buscar partida
+ */
