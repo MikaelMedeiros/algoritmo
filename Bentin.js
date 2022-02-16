9a5ab88f3d2cfd57f5418ed36defb62f7bab4600
Bentox <bentobentobentobento@gmail.com>
2022-02-16 20:05:25 -0300

    bento vestindo roupa

---
+++ Bentin.js
@@ -0,0 +1,10 @@
+/**
+ Algoritmo para vestir uma roupa
+ 1. Estar pelado
+ 2. Coloca a cuequinha
+ 3. Coloco bermuda
+ 4. coloco o chinelo
+ 5. escolho e coloca uma camiseta
+ 6. coloco os óculos
+ 7. coloco o relógio
+ */
