8651bacb3f8ccc17dc84e52a347071ff0d3a3f64
Bentox <bentobentobentobento@gmail.com>
2022-02-16 20:33:11 -0300

    regando a planta do lucas

--- anotacoes.js
+++ anotacoes.js
@@ -6,3 +6,10 @@
  * 5 - colocar sapato
  * 6 - lucas otario
  */
+/**
+ * algoritmo para regar uma planta
+ * 1 - Pegar um regador
+ * 2 - encher de agua
+ * 3 - selecionar a planta a ser regada
+ * 4 - regar a planta selecionada  
+ */
