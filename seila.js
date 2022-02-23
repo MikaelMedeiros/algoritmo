6d0531129e0242dd4758970fdb5543b6a15b76bc
SandroMix <sandro.226@hotmail.com>
2022-02-23 19:56:05 -0300

    algoritmo regar plantas

--- seila.js
+++ seila.js
@@ -3,7 +3,14 @@
  * vestir camisa
  * vestir meia
  * calçar tenis
+ */
+/**
+ * regar plantas
  * 
- * 
- * 
+ * pegar vasilha
+ * por agua na vasilha
+ * ir ate a planta
+ * molhar
+ * jogar resto da agua fora
+ * guardar vasilha
  */
